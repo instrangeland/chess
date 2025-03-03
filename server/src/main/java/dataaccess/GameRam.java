96b2c575a787c0ac2add9139aa12e7cd17608415
ethan <ut2mi2013@gmail.com>
2025-03-02 18:58:17 -0700

    game service & DAO

--- server/src/main/java/dataaccess/GameRam.java
+++ server/src/main/java/dataaccess/GameRam.java
@@ -12,25 +12,31 @@ public class GameRam implements GameDAO{
     static int nextGameNum = 0;
     @Override
     public int createGame(String gameName) {
-        ChessGame game = new ChessGame();
-        gameDataArrayList.set(nextGameNum, new GameData(nextGameNum, null, null, gameName, game));
         nextGameNum++;
-        return nextGameNum-1;
+        ChessGame game = new ChessGame();
+        gameDataArrayList.add(new GameData(nextGameNum, null, null, gameName, game));
+        return nextGameNum;
     }
 
     @Override
     public GameData getGame(int gameID) {
-        return gameDataArrayList.get(gameID);
+        return gameDataArrayList.get(gameID-1);
     }
 
     @Override
     public void updateGameData(int gameID, GameData gameData) {
-        gameDataArrayList.set(gameID, gameData);
+        gameDataArrayList.set(gameID-1, gameData);
     }
 
     @Override
     public List<GameData> listGames() {
         return gameDataArrayList;
+    }
+
+    public void clear() {
+        System.out.println("clearing games");
+        gameDataArrayList.clear();
+        nextGameNum = 0;
     }
 
 }
--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -3,15 +3,38 @@ package service;
 import dataaccess.AuthDAO;
 import dataaccess.GameDAO;
 import dataaccess.GameRam;
+import model.GameData;
+import response.ListGamesResponse;
+import response.NewGameResponse;
 
 public class GameService {
     public static void setGameDAO(GameDAO gameDAO) {
         GameService.gameDAO = gameDAO;
     }
 
+    public static void updateGame(int gameNum, GameData newData) {
+        gameDAO.updateGameData(gameNum, newData);
+    }
+
+    public static GameData getGame(int gameNum) {
+        System.out.println("getting game "+gameNum);
+        return gameDAO.getGame(gameNum);
+    }
+
     static private GameDAO gameDAO;
 
     static public void deleteGames() {
-
+        gameDAO.clear();
     }
+
+    static public ListGamesResponse listGames() {
+        return new ListGamesResponse(gameDAO.listGames());
+    }
+
+    static public NewGameResponse newGame(String gameName) {
+        NewGameResponse game = new NewGameResponse(gameDAO.createGame(gameName));
+        System.out.println("made new game "+game.gameID());
+        return game;
+    }
+
 }
