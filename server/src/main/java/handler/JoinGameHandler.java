0ccf2ed73f3024d9044f1dd42a7dc2df6d7e5d34
ethan <ut2mi2013@gmail.com>
2025-03-02 18:59:03 -0700

    join game handler

--- server/src/main/java/handler/JoinGameHandler.java
+++ server/src/main/java/handler/JoinGameHandler.java
@@ -1,5 +1,14 @@
 package handler;
 
+import com.google.gson.Gson;
+import error.BadRequestError;
+import error.TakenError;
+import error.UnauthorizedError;
+import model.GameData;
+import request.JoinGameRequest;
+import request.NewGameRequest;
+import service.AuthService;
+import service.GameService;
 import spark.Request;
 import spark.Response;
 
@@ -7,6 +16,46 @@ public class JoinGameHandler extends Handler{
 
     @Override
     public Object handleRequest(Request req, Response res) {
-        return null;
+        res.type("application/json");
+        Gson gson = new Gson();
+        String authToken = req.headers("authorization");
+        if (authToken == null) {
+            throw new UnauthorizedError();
+        }
+        AuthService.auth(authToken);
+        String username = AuthService.getUsername(authToken);
+        JoinGameRequest joinGameRequest = gson.fromJson(req.body(), JoinGameRequest.class);
+        if (joinGameRequest.playerColor() == null) {
+            throw new BadRequestError();
+        }
+        if (!joinGameRequest.playerColor().equals("BLACK") && !joinGameRequest.playerColor().equals("WHITE")) {
+            throw new BadRequestError();
+        }
+        try {
+            GameData data = GameService.getGame(joinGameRequest.gameID());
+            if (data == null) {
+                throw new BadRequestError();
+            }
+            GameData newData;
+            if (joinGameRequest.playerColor().equals("BLACK")) {
+                if (data.blackUsername() == null) {
+                    newData = new GameData(data.gameID(), data.whiteUsername(), username, data.gameName(), data.game());
+                } else {
+                    throw new TakenError();
+                }
+            } else {
+                if (data.whiteUsername() == null) {
+                    newData = new GameData(data.gameID(), username, data.blackUsername(), data.gameName(), data.game());
+                } else {
+                    throw new TakenError();
+                }
+            }
+            GameService.updateGame(joinGameRequest.gameID(), newData);
+        } catch (IndexOutOfBoundsException e) {
+            throw new BadRequestError();
+        }
+
+
+        return "{}";
     }
 }
