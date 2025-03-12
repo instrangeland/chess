7c5484a0183fa38db2f836cf52a57524baf66237
ethan <ut2mi2013@gmail.com>
2025-03-11 23:29:34 -0600

    first two gamedao tests

--- server/src/test/java/dataaccess/GameDAOTests.java
+++ server/src/test/java/dataaccess/GameDAOTests.java
@@ -2,9 +2,11 @@ package dataaccess;
 
 import dataaccess.*;
 import dataaccess.SQL.AuthSQL;
+import dataaccess.SQL.GameSQL;
 import error.ResponseError;
 import error.UnauthorizedError;
 import model.AuthData;
+import model.GameData;
 import org.junit.jupiter.api.*;
 import request.LoginRequest;
 import request.LogoutRequest;
@@ -16,4 +18,37 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GameDAOTests {
+    static GameDAO gameDAO;
+
+    @BeforeAll
+    public static void init() {
+        gameDAO = new GameSQL();
+        gameDAO.clear();
+
+    }
+
+    @AfterAll
+    public static void clean() {
+        gameDAO.clear();
+
+    }
+
+    @Test
+    @Order(0)
+    @DisplayName("Check createGame")
+    public void checkCreateGame() throws Exception {
+        int gameNum = gameDAO.createGame("abc");
+        GameData data = gameDAO.getGame(gameNum);
+        assertEquals("abc", data.gameName());
+    }
+
+    @Test
+    @Order(0)
+    @DisplayName("Check createGame fails")
+    public void checkCreateGameFails() throws Exception {
+        assertThrows(ResponseError.class, () -> gameDAO.createGame(null));
+
+    }
+
+
 }
