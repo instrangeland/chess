df48ac744af71426c2375a927ce380a483b59b59
ethan <ut2mi2013@gmail.com>
2025-03-21 21:03:29 -0600

    setup for lab 5, added repl.java

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,19 @@
 import chess.*;
+import ui.Repl;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
+        URI uri = new URI("http://localhost:8080");
+        if (args.length == 1) {
+            uri = new URI(args[0]);
+        }
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        new Repl(uri).run();
+
     }
 }
---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,13 @@
+package ui;
+
+import java.net.URI;
+
+public class Repl {
+    public Repl(URI uri) {
+
+
+    }
+    public void run() {
+
+    }
+}
