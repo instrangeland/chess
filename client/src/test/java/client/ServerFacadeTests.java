efde80baa09dea200f40cc7f207f8b3c76ad02e1
ethan <ut2mi2013@gmail.com>
2025-03-24 18:06:21 -0600

    wrote clear and register test.

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,29 +1,110 @@
 package client;
 
+import error.ResponseError;
+import error.UnauthorizedError;
+import exception.ResponseException;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFascade;
+import service.AuthService;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class ServerFacadeTests {
-
     private static Server server;
+    private static ServerFascade serverFascade;
+    private static int game1ID;
+    private static int game2ID;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        String url = "http://localhost/"+port;
     }
 
-    @AfterAll
-    static void stopServer() {
-        server.stop();
+    @BeforeEach
+    public void setup() {
+        game1ID = serverFascade.createGame("game1");
+        game2ID = serverFascade.createGame("game2");
+        serverFascade.register(new UserData("abc", "123", "hi"));
+        serverFascade.register(new UserData("def", "123", "hi"));
     }
+
+    @AfterEach
+    public void resetServer() {
+        serverFascade.clear();
+    }
+
+
+//    @AfterAll
+//    static void stopServer() {
+//        server.stop();
+//    }
+
+
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @Order(0)
+    @DisplayName("Check register works")
+    public void checkRegister() throws Exception {
+        assertDoesNotThrow(() -> serverFascade.register("fed", "123", "hi")));
     }
 
+    @Test
+    @Order(1)
+    @DisplayName("Check register fails")
+    public void checkRegisterFails() throws Exception {
+        assertThrows(ResponseException.class, () -> serverFascade.register(new UserData("def", "123", "hi")));
+    }
+
+    @Test
+    @Order(0)
+    @DisplayName("Check clear works")
+    public void checkClear() throws Exception {
+        assertDoesNotThrow(() -> AuthService.auth(auth.authToken()));
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Check auth fails")
+    public void checkClearNoFail() throws Exception {
+        assertThrows(UnauthorizedError.class, () -> AuthService.auth("abc"));
+    }
+
+    @Test
+    @Order(0)
+    @DisplayName("Check clear works")
+    public void checkClear() throws Exception {
+        assertDoesNotThrow(() -> AuthService.auth(auth.authToken()));
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Check auth fails")
+    public void checkClearNoFail() throws Exception {
+        assertThrows(UnauthorizedError.class, () -> AuthService.auth("abc"));
+    }
+    }
+
+    @Test
+    @Order(0)
+    @DisplayName("Check clear works")
+    public void checkClear() throws Exception {
+        assertDoesNotThrow(() -> AuthService.auth(auth.authToken()));
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Check clear fails when server is off")
+    public void checkClearNoFail() throws Exception {
+        server.stop();
+        assertThrows(ResponseException.class, () -> serverFascade::clear(););
+    }
+    
 }
